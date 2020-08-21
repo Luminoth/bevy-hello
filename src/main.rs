ae1f6ec7bc4badcf96687c6c093891f85dc15ea4
Shane <ignignokterr@gmail.com>
2020-08-20 17:47:59 -0700

    Clear color

--- src/main.rs
+++ src/main.rs
@@ -6,10 +6,12 @@ mod prefabs;
 mod systems;
 
 use bevy::prelude::*;
+use bevy::render::pass::ClearColor;
 
 fn main() {
     App::build()
         .add_default_plugins()
+        .add_resource(ClearColor(Color::rgb(0.0, 0.0, 0.0)))
         .add_plugin(plugins::HelloPlugin)
         .run();
 }
